e65883ff3ec5d5d216054fd61dcf552278c4537f
bala <dmbala@gmail.com>
2016-05-11 12:17:15 -0500

    edit curl script

--- get_list_of_condor_history.bash
+++ get_list_of_condor_history.bash
@@ -1,2 +1,3 @@
 #!/bin/bash
-curl -XGET 'uct2-es-head.mwt2.org:9200/_cat/indices?v' | grep "condor_history-2016.2"
+#Displays the relevant indices that have a string "condor_history-2016"
+curl -XGET 'uct2-es-head.mwt2.org:9200/_cat/indices?v' | grep "condor_history-2016"
